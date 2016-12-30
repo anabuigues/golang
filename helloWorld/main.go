8893f92a6b434ff204313c2ddbcbe5571ae354bd
Ana Buigues Grimalt <anavanesa.buigues.grimalt@bbva.com>
2016-12-30 14:31:32 +0100

    use printf

--- helloWorld/main.go
+++ helloWorld/main.go
@@ -1,4 +1,4 @@
-package helloWorld
+package main
 
 import (
 	"fmt"
@@ -6,5 +6,10 @@ import (
 
 func main() {
 	fmt.Println("Hello, playground")
+	fmt.Println(42)
+	fmt.Printf("%d - %b\n", 42, 42)
+	fmt.Printf("%d - %b - %x\n", 42, 42, 42)
+	fmt.Printf("%d - %b - %#x\n", 42, 42, 42)
+	fmt.Printf("%d \t %b \t %#x\n", 42, 42, 42)
 
 }
