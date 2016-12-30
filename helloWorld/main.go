3f21fe722287b6804c48d79a474024c7300d3dc7
Ana Buigues Grimalt <anavanesa.buigues.grimalt@bbva.com>
2016-12-30 14:43:25 +0100

    add loop for

--- helloWorld/main.go
+++ helloWorld/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
@@ -11,5 +12,10 @@ func main() {
 	fmt.Printf("%d - %b - %x\n", 42, 42, 42)
 	fmt.Printf("%d - %b - %#x\n", 42, 42, 42)
 	fmt.Printf("%d \t %b \t %#x\n", 42, 42, 42)
+	fmt.Println(math.Pi)
+
+	for i := 0; i < 200; i++  {
+		fmt.Printf("%d \t %b \t %x %q \n", i, i, i, i)
+	}
 
 }
