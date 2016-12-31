08fcbda908ab31433461c6888cf784c8e6a976bd
Ana Buigues Grimalt <anavanesa.buigues.grimalt@bbva.com>
2016-12-31 17:45:22 +0100

    Understanding constants and iota

--- 06_constants/02_multiple-initialization/main.go
+++ 06_constants/02_multiple-initialization/main.go
@@ -3,14 +3,19 @@ package main
 import "fmt"
 
 const (
+	//untyped
 	pi       = 3.14
 	language = "Go"
+	// typed
+	name string = "Ana"
 )
 
 func main() {
 
 	fmt.Println(pi)
 	fmt.Println(language)
+	fmt.Println(name)
 	fmt.Printf("%T \n", pi)
 	fmt.Printf("%T \n", language)
+	fmt.Printf("%T \n", name)
 }
